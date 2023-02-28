fe7b563730d3de23f54dffd1081405f8c89c80fa
Wanyu Jiang <wanyu19930930@gmail.com>
2023-02-28 12:20:22 +0900

    Generate Stimulus controller

--- app/controllers/bins_controller.rb
+++ app/controllers/bins_controller.rb
@@ -1,2 +1,11 @@
 class BinsController < ApplicationController
+  def index
+    @bins = Bin.all
+    @markers = @bins.geocoded.map do |bin|
+      {
+        lat: bin.latitude,
+        lng: bin.longitude
+      }
+    end
+  end
 end
