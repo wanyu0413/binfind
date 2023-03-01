3fd5ccffdf8f964f2f5a8936ff1cb40d5079f440
Julien Afonso <ju.afonso61@gmail.com>
2023-03-01 15:20:12 +0900

    associatons-validations

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -2,6 +2,8 @@ class Bin < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
+  has_many :bin_categories
+  has_many :categories, through: :bin_categories
   has_many_attached :photos
-  validates :category, :name, :open_time, :end_time, presence: true
+  validates :name, :open_time, :end_time, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :bin_categories
+  has_many :bins, through: :bin_categories
+  validates :name, presence: true
 end
