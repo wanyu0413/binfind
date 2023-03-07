14f643ddb63a6cc1c8d2e9af9eef77eab920e573
Ruka Okuyama <rukaokuyama@gmail.com>
2023-03-07 15:02:32 +0900

    changes to filter

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,13 @@ class Category < ApplicationRecord
   has_many :bin_categories
   has_many :bins, through: :bin_categories
   validates :name, presence: true
+  def icon
+    case name
+    when "burnable" then "fa-dumpster-fire"
+    when "pet bottle" then "fa-wine-bottle"
+    when "unburnable" then "fa-circle-xmark"
+    else
+      "fa-prescription-bottle"
+    end
+  end
 end
