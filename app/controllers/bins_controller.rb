f2cf3985c28f3a9cdb1815988084657716b0b434
Ruka Okuyama <rukaokuyama@gmail.com>
2023-03-08 12:43:33 +0900

    add new bin popup

--- app/controllers/bins_controller.rb
+++ app/controllers/bins_controller.rb
@@ -6,6 +6,7 @@ class BinsController < ApplicationController
 
   def index
     @bins = policy_scope(Bin)
+    @bin = Bin.new
     if params[:query].present?
       # @bins = Bin.where(category: params[:query])
       @bins = Bin.joins(bin_categories: :category).where(categories: {name: params[:query]})
@@ -21,11 +22,6 @@ class BinsController < ApplicationController
   end
 
   def show
-    authorize @bin
-  end
-
-  def new
-    @bin = Bin.new
     authorize @bin
   end
 
